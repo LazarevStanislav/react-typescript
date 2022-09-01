cd64742974504fbe4aec7324e361f607d13276ae
Stanislav <lazarev@yandex.ru>
2022-09-01 23:25:15 +0300

    create todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ITodo } from './components/interfaces';
 import { Navbar } from './components/Navbar/Navbar';
 import { TodoForm } from './components/TodoForm/TodoForm';
+import { TodoList } from './components/TodoList/TodoList';
 
 
 const App: React.FC = () => {
+  const [todos, setTodos] = useState<ITodo[]>( [] )
+  
+  const addHandler = ( title: string ) => {
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false
+    }
+    
+    setTodos( prev => [newTodo, ...prev] )
+    
+  }
+
   return (
   <>
       <Navbar></Navbar>
       <div className="container">
-        <TodoForm />
+        <TodoForm onAdd={addHandler} />
+        <TodoList todos={todos} />
       </div>
   </>
   )
--- src/components/TodoForm/TodoForm.tsx
+++ src/components/TodoForm/TodoForm.tsx
@@ -1,6 +1,10 @@
 import React, { useRef, useState } from 'react'
 
-export const TodoForm: React.FC = () => {
+interface TodoFormProps {
+  onAdd(title: string): void
+}
+
+export const TodoForm: React.FC<TodoFormProps> = (props) => {
   // const [title, setTitle] = useState<string>( '' )
   const ref = useRef<HTMLInputElement>(null)
   
@@ -12,7 +16,7 @@ export const TodoForm: React.FC = () => {
     if ( event.key === 'Enter' ) {
       // console.log(title);
       // setTitle('')
-      console.log(ref.current!.value);
+      props.onAdd(ref.current!.value);
       ref.current!.value = ''
     }
   }
